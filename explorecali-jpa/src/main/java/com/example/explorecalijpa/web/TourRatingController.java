40add06dd721638bcb0e842cb038c24cc5491537
tnais <gianuberto.lauri@gmail.com>
2025-10-27 17:10:58 +0100

    #04_01 - salvati
    Changes to be committed:
    	modified:   pom.xml
    	modified:   src/main/java/com/example/explorecalijpa/web/TourRatingController.java

--- explorecali-jpa/src/main/java/com/example/explorecalijpa/web/TourRatingController.java
+++ explorecali-jpa/src/main/java/com/example/explorecalijpa/web/TourRatingController.java
@@ -1,22 +1,37 @@
 package com.example.explorecalijpa.web;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.explorecalijpa.business.TourRatingService;
 
+import java.util.NoSuchElementException;
+
 /**
  * Tour Rating Controller
- *
  * Created by Mary Ellen Bowman
  */
 @RestController
 @RequestMapping(path = "/tours/{tourId}/ratings")
 public class TourRatingController {
-  private TourRatingService tourRatingService;
+  private final TourRatingService tourRatingService;
 
   public TourRatingController(TourRatingService tourRatingService) {
     this.tourRatingService = tourRatingService;
   }
-  
+
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public void createTourRating(@PathVariable(value = "tourId") int tourId,
+                               @RequestBody @Valid RatingDto ratingDto) {
+      tourRatingService.createNew(tourId, ratingDto.getCustomerId(), ratingDto.getScore(), ratingDto.getComment());
+  }
+
+  @ExceptionHandler(NoSuchElementException.class)
+  @ResponseStatus(HttpStatus.NOT_FOUND)
+  public String handleNoSuchElementException(NoSuchElementException e) {
+      return e.getMessage();
+  }
+
 }
